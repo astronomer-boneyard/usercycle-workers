a40b30fc5d05cd6ccb17baee69d94310e870f23d
schnie <greg@differential.com>
2015-05-01 14:00:42 -0400

    dist lua scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 var gulp = require('gulp'),
     babel = require('gulp-babel'),
     nodemon = require('gulp-nodemon'),
-    del = require('del');
+    del = require('del'),
+    runSequence = require('run-sequence');
+
+var copy = [
+  'lua/**'
+];
 
 var settings = {
   dist: 'dist'
 };
 
 gulp.task('clean', function() {
-  del(settings.dist + '/*');
+  return del.sync(settings.dist + '/*');
+});
+
+gulp.task('copy', function() {
+  return gulp.src(copy, {base: '.'})
+    .pipe(gulp.dest(settings.dist));
 });
 
 gulp.task('babel', function () {
@@ -17,12 +27,16 @@ gulp.task('babel', function () {
     .pipe(gulp.dest(settings.dist));
 });
 
-gulp.task('watch', ['clean', 'babel'], function() {
+gulp.task('build', function(cb) {
+  runSequence('clean', ['copy', 'babel'], cb);
+})
+
+gulp.task('watch', ['clean', 'copy', 'babel'], function() {
   nodemon({
     script: settings.dist + '/app.js',
     ext: 'js',
     ignore: [settings.dist + '/*'],
-    tasks: ['clean', 'babel'],
+    tasks: ['clean', 'copy', 'babel'],
     env: {
       'NODE_ENV': 'development'
     }
@@ -30,5 +44,3 @@ gulp.task('watch', ['clean', 'babel'], function() {
     console.log('Retarted server...');
   });
 });
-
-gulp.task('build', ['clean', 'babel']);
